1fd118eef31b6adc279dc76cb26b5eb5899231f7
vladis149@gmail.com <vpyven@brujula.es>
2019-04-09 12:04:02 +0200

    creado objeo Fighter para guardar estado

---
+++ Ejercicio1/src/Fighter.java
@@ -0,0 +1,37 @@
+public class Fighter {
+    private String Name;
+    private int Health, Damage;
+
+    //instanciar valores del estado del fighter
+    public Fighter (){
+        Name="";
+        Health=0;
+        Damage=0;
+    }
+    //definir estado del fighter
+    public Fighter(String name, int health, int damage){
+        Name=name;
+        Health=health;
+        Damage=damage;
+    }
+    //obtener estado
+    public String getName(){return Name;}
+    public int getHealth(){return Health;}
+    public int getDamage(){return Damage;}
+
+    //guardar estado
+    public void setName(String name){Name = name;}
+    public void setHealth(int health){Health = health;}
+    public void setDamage(int damage){Damage = damage;}
+
+    //mostrar estado
+    @Override
+    public String toString() {
+        return "Fighter{" +
+                "Name='" + Name + '\'' +
+                ", Health=" + Health +
+                ", Damage=" + Damage +
+                '}';
+    }
+
+}
