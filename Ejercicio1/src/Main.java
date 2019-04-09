67ca2343324cffa4b749e702eee49f3c4a6aea8c
vladis149@gmail.com <vpyven@brujula.es>
2019-04-09 12:58:18 +0200

    creado en Main escenario donde luchan

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
-    public static void main (String [] args){
+    public static void main (String [] args)throws Exception{
+        //creando a los Fighters
+        Fighter fighterRed = new Fighter("Red",4,2);
+        Fighter fighterBlue = new Fighter("Blue",1,3);
 
+        //
+        try{
+            while(fighterRed.getHealth()>0 && fighterBlue.getHealth()>0){
 
+                fighterBlue.setHealth(fighterBlue.getHealth()-fighterRed.getDamage());
+                if(fighterBlue.getHealth()>0){
+                    System.out.println("ENTRA EN IF");
+                fighterRed.setHealth(fighterRed.getHealth()-fighterBlue.getDamage());
+                }
 
+                System.out.println("Estado vida Azul: "+fighterBlue.getHealth());
+                System.out.println("Estado vida Rojo: "+fighterRed.getHealth());
 
-
+            }
+        }catch(Exception e){
+            System.out.println("Error: "+e.toString());
+        }
     }
+    
+
 }
