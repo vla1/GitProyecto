6728806f2f0af9e06b2d65513defb8f9009557e0
vla1 <vladis149@gmail.com>
2019-04-10 11:25:59 +0200

    version mejorada con una funcion de comprobar vida

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -1,19 +1,22 @@
 public class Main {
     public static void main (String [] args)throws Exception{
         //creando a los Fighters
-        Fighter fighterRed = new Fighter("Red",4,2);
-        Fighter fighterBlue = new Fighter("Blue",1,3);
+        Fighter fighterRed = new Fighter("Red",10,2);
+        Fighter fighterBlue = new Fighter("Blue",10,3);
 
-        //
+
         try{
-            while(fighterRed.getHealth()>0 && fighterBlue.getHealth()>0){
 
+            while((statusHealthFighter(fighterRed.getHealth())) && (statusHealthFighter(fighterBlue.getHealth()))){
+
+                //azul recibe daño del rojo
                 fighterBlue.setHealth(fighterBlue.getHealth()-fighterRed.getDamage());
-                if(fighterBlue.getHealth()>0){
-                    System.out.println("ENTRA EN IF");
-                fighterRed.setHealth(fighterRed.getHealth()-fighterBlue.getDamage());
+                //ver si el azul tiene vida
+                if(statusHealthFighter(fighterBlue.getHealth())){
+                    //rojo recibe dañ3o
+                    fighterRed.setHealth(fighterRed.getHealth()-fighterBlue.getDamage());
                 }
-
+                //mostrar estados de azul y verde
                 System.out.println("Estado vida Azul: "+fighterBlue.getHealth());
                 System.out.println("Estado vida Rojo: "+fighterRed.getHealth());
 
@@ -22,6 +25,20 @@ public class Main {
             System.out.println("Error: "+e.toString());
         }
     }
-    
+    public static boolean statusHealthFighter (int vida){
+        System.out.println("vidaFuncion "+vida);
+        //si tiene vida
+        if(vida>0) {
+
+            return true;
+        }
+        //
+        return false;
+    }
+    public static void statusShow (){
+
+    }
+
+
 
 }
