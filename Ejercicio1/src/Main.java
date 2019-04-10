109c55197a621db9274ad107a76ddc5181f9bde5
vla1 <vladis149@gmail.com>
2019-04-10 11:54:20 +0200

    version2 mejorada con funciones visualizar estado y resultado

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -4,41 +4,53 @@ public class Main {
         Fighter fighterRed = new Fighter("Red",10,2);
         Fighter fighterBlue = new Fighter("Blue",10,3);
 
-
         try{
 
             while((statusHealthFighter(fighterRed.getHealth())) && (statusHealthFighter(fighterBlue.getHealth()))){
-
                 //azul recibe daño del rojo
                 fighterBlue.setHealth(fighterBlue.getHealth()-fighterRed.getDamage());
                 //ver si el azul tiene vida
                 if(statusHealthFighter(fighterBlue.getHealth())){
-                    //rojo recibe dañ3o
+                    //rojo recibe daño
                     fighterRed.setHealth(fighterRed.getHealth()-fighterBlue.getDamage());
                 }
                 //mostrar estados de azul y verde
-                System.out.println("Estado vida Azul: "+fighterBlue.getHealth());
-                System.out.println("Estado vida Rojo: "+fighterRed.getHealth());
-
+                statusShow(fighterBlue.getHealth(),fighterRed.getHealth());
             }
+            resultBattle(fighterBlue,fighterRed);
+
         }catch(Exception e){
             System.out.println("Error: "+e.toString());
         }
     }
+
     public static boolean statusHealthFighter (int vida){
-        System.out.println("vidaFuncion "+vida);
         //si tiene vida
         if(vida>0) {
-
             return true;
         }
-        //
+        // si la vida es <=0
         return false;
     }
-    public static void statusShow (){
 
+    public static void statusShow (int statusBlue, int statusRed){
+        System.out.println("Estado del luchador Azul: "+statusBlue);
+        System.out.println("Estado del luchador Rojo: "+statusRed);
+        System.out.println("-----------------------");
     }
 
+    public static void resultBattle(Fighter fighterBlue, Fighter fighterRed){
+        if(fighterBlue.getHealth()>0){
+            System.out.println("***************");
+            System.out.println("EL GANADOR ES: "+fighterBlue.getName()+"\nDAÑO POR ATAQUE: "+fighterBlue.getDamage()+"\nVIDA RESTANTE: "+fighterBlue.getHealth());
+            System.out.println("***************");
+        }else{
+            System.out.println("***************");
+            System.out.println("EL GANADOR ES: "+fighterRed.getName()+"\nDAÑO POR ATAQUE: "+fighterRed.getDamage()+"\nVIDA RESTANTE: "+fighterRed.getHealth());
+            System.out.println("***************");
+        }
 
 
+
+    }
 }
